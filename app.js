73a6665e2d8799721af2d08809662b2eae9e6ce5
Philip R <rogersph@otc.edu>
2018-09-04 21:06:25 -0500

    Updated app.js
    Expanded app.js to also include solutions to Tutorial "Play With Variables"

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+//1, 2, 3, Code!
+
 //Name and Age
 console.log("Philip Rogers");
 console.log("26");
@@ -19,3 +21,16 @@ console.log(6/3);
 console.log(4 + 5);
 console.log("4 + 5");
 console.log("4" + "5");
+
+
+
+//Play With Variables
+
+//Celsius to Fahrenheit
+let degF, degC;
+degC = 32;
+
+degF = (degC * (9/5)) + 32;
+
+console.log("Temperature in Fahrenheit:");
+console.log(degF);
