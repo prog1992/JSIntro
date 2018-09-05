b95a241a1f57a282d0db644707b33820094bffee
Philip R <rogersph@otc.edu>
2018-09-04 21:02:19 -0500

    Created app.js
    Created app.js file with solutions for 1, 2, 3, Code! tutorial

---
+++ app.js
@@ -0,0 +1,21 @@
+//Name and Age
+console.log("Philip Rogers");
+console.log("26");
+
+//Simplistic Calculator
+console.log("6 + 3 = ?");
+console.log(6+3);
+
+console.log("6 - 3 = ?");
+console.log(6-3);
+
+console.log("6 x 3 = ?");
+console.log(6*3);
+
+console.log("6 / 3 = ?");
+console.log(6/3);
+
+//Values Prediction
+console.log(4 + 5);
+console.log("4 + 5");
+console.log("4" + "5");
